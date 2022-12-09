5adda5ec23b0dfab047f3ad667d3b7ae7c3069ac
wangruting <wangruting@baidu.com>
2022-12-09 12:11:39 +0000

    modify Volterra_IDE

--- deepxde/backend/pytorch/tensor.py
+++ deepxde/backend/pytorch/tensor.py
@@ -211,4 +211,4 @@ def roll(tensor, shift, axis=None):
 
 
 def gradients(x, y):
-    return torch.autograd.grad(x, y, create_graph=True)
+    return torch.autograd.grad(x, y,torch.ones_like(y, memory_format=torch.preserve_format),retain_graph=True)
