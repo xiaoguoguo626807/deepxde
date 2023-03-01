d59584cd88c671107d71253d97345922fc67ad9b
wangruting <wangruting@baidu.com>
2023-03-01 11:26:58 +0000

    add Eular_beam

--- examples/pinn_forward/Euler_beam.py
+++ examples/pinn_forward/Euler_beam.py
@@ -2,7 +2,8 @@
 import deepxde as dde
 import numpy as np
 import paddle
-paddle.enable_static()
+from paddle.fluid import core
+core.__set_bwd_prim_enabled(True)
 
 def ddy(x, y):
     return dde.grad.hessian(y, x)
